0b45b6fd139dc95a84b5293f674ff0b62c6ab588
Alexey Shchur <hi@alexeyshchur.com>
2025-09-22 08:52:45 +0300

    fix: exports

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export {
+  enrichTraceWithCallAndEventDetails,
+  ReceiptLog,
+  stitchLogsIntoCallTrace,
+} from "./lib/stitch-receipt-logs-into-calltracer";
+
+export type { TraceCall, LogLike } from "./types";
